c2a969998a31d9b3726893d8f622b696405f18e8
Bikash <techbmb10@gmail.com>
2022-06-17 14:56:23 +0545

    ecoomerce 3rd day

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:ecommerce/screens/buttom_bar.dart';
+import 'package:ecommerce/screens/cart.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // const ({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BottomBarScreen(),
+    );
+  }
+}
